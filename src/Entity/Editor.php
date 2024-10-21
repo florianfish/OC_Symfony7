6461fa6984156076aad0bd90ce09dbf815cf7755
Florian <florian@poissonmail.fr>
2024-10-21 16:08:48 +0200

    fix api

--- src/Entity/Editor.php
+++ src/Entity/Editor.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\EditorRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -9,6 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: EditorRepository::class)]
+#[ApiResource()]
 class Editor
 {
     #[ORM\Id]
